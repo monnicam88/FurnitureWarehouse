7223aba5c8db2c16a97c4221fce71a2767847aa5
monnica_88 <72393725+monnicam88@users.noreply.github.com>
2023-03-21 21:26:46 +0200

    Update and rename Подзаявки.sql to Subquieries.sql

--- Subquieries.sql
+++ Subquieries.sql
@@ -1,16 +1,9 @@
--- Моника Митрева, №62522 --
-
--- Подзаявки
-
--- №1. Да се изведе кодът на производителя на всички кръгли маси.
 SELECT items.manufacturer_code AS MANUFACTURER_CODE
 	FROM ITEM items
 WHERE items.code IN (SELECT tables_.item_code
 						FROM TABLE_ tables_
 					WHERE tables_.type = 'oval');
-
--- №2. Да се изведат името, държавата и годината на основаване на 
--- фирмите производители на столове.
+					
 SELECT manufacturers.name AS MANUFACTURER_NAME,
 	   manufacturers.county AS COUNTRY,
 	   manufacturers.year AS YEAR_OF_FOUNDATION
@@ -19,7 +12,6 @@ WHERE manufacturers.code IN (SELECT items.manufacturer_code
 								FROM ITEM items
 							WHERE items.type = 'Chair');
 
--- №3. Да се изведе вида на най-скъпия артикул в склада и неговата цена.
 SELECT items.type AS ITEM_TYPE, 
 	   products.price AS ITEM_PRICE
 	FROM ITEM items, (SELECT sofas.item_code, sofas.price
@@ -40,7 +32,6 @@ WHERE items.code = products.item_code
 							   SELECT price
 							      FROM TABLE_);
 
--- №4. Да се изведе името на магазина, получил последна доставка от склада.
 SELECT name AS STORE_NAME
 	FROM STORE
 WHERE code IN (SELECT store_code
@@ -48,7 +39,6 @@ WHERE code IN (SELECT store_code
 			   WHERE delivery_date >= ALL (SELECT delivery_date 
 											FROM DELIVERY));
 
--- №5. Да се изведат номерата на пакетите, в които има кожени двуместни дивани.
 SELECT bundle_id AS BUNDLE_NUMBER
 	FROM BUNDLEITEM 
 WHERE item_code IN (SELECT items.code
