fb1e63d126e9d0c9c19c2e114812ec135163085b
monnica_88 <72393725+monnicam88@users.noreply.github.com>
2023-03-21 21:30:15 +0200

    Update and rename Прости заявки.sql to Simple queries.sql

--- Simple queries.sql
+++ Simple queries.sql
@@ -1,31 +1,20 @@
--- Моника Митрева, №62522 --
-
--- Прости заявки
-
--- №1. Да се изведат кодът и цената на всички дивани, които са под 500 лева.
 SELECT sofas.item_code AS SOFA_CODE, 
 	   sofas.price AS PRICE
 	FROM SOFA sofas
 WHERE sofas.price < 500;
 
--- №2. Да се изведат имената (и годината) на всчики български производители, 
--- основани след 1990 година.
 SELECT manufacturers.name AS MANUFACTURER, 
 	   manufacturers.year AS YEAR_OF_FOUNDATION
 	FROM MANUFACTURER manufacturers
 WHERE manufacturers.county = 'Bulgaria'
 	AND manufacturers.year > 1990;
 
--- №3. Да се изведе информацията за всички мебелни магазини, с които складът 
--- работи.
 SELECT stores.code AS CODE,
 	   stores.name AS NAME,
 	   stores.address AS ADDRESS
 	FROM STORE stores
 WHERE stores.type = 'furniture store';
 
--- №4. Да се изведе информацията за всички локации, чийто код започва с "SB".
--- SELECT *
 SELECT locations.code AS CODE,
 	   locations.sector AS SECTOR,
 	   locations.row AS ROW,
@@ -33,8 +22,6 @@ SELECT locations.code AS CODE,
 	FROM LOCATION locations
 WHERE locations.code like ('SB%');
 
--- №5. Да се изведат цените на всички столове, които са червени или черни на 
--- цвят и струват по-малко от 250 лева.
 SELECT chairs.price AS PRICE
 	FROM CHAIR chairs
 WHERE (chairs.colour = 'Red' OR chairs.colour = 'Black')
